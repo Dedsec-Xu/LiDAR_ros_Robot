66ebb47186987eceee8608c3f2dec1e644413d96
Dedsec-Xu <xld1998615@vip.qq.com>
2019-06-27 11:12:00 +0800

    Update user_gpio_i2c.c
    format

--- BSP/user_gpio_i2c.c
+++ BSP/user_gpio_i2c.c
@@ -1,7 +1,8 @@
 #include "user_gpio_i2c.h"
+
 GPIO_TypeDef *gpio_i2c_scl_gpiox;
 uint16_t gpio_i2c_scl_pin;
-GPIO_TypeDef* gpio_i2c_sda_gpiox;
+GPIO_TypeDef *gpio_i2c_sda_gpiox;
 uint16_t gpio_i2c_sda_pin;
 
 #define IIC_SCL_LOW     (gpio_i2c_scl_gpiox->BRR = gpio_i2c_scl_pin)
@@ -9,12 +10,22 @@ uint16_t gpio_i2c_sda_pin;
 #define IIC_SDA_LOW     (gpio_i2c_sda_gpiox->BRR = gpio_i2c_sda_pin)
 #define IIC_SDA_HIGH    (gpio_i2c_sda_gpiox->BSRR = gpio_i2c_sda_pin)
 
-#define READ_SDA        (gpio_i2c_sda_gpiox->IDR & gpio_i2c_sda_pin)
 
-#define SDA_IN() {SDA_GPIO_Port->CRH &= 0XFFFF0FFF; SDA_GPIO_Port->CRH|=8<<12;}
+#define READ_SDA        (gpio_i2c_sda_gpiox->IDR &gpio_i2c_sda_pin)
 
-#define SDA_OUT() {SCL_GPIO_Port->CRH &= 0XFFFF0FFF; SCL_GPIO_Port->CRH|=3<<12;}
 
+#define SDA_IN()
+{
+    SDA_GPIO_Port->CRH &= 0XFFFF0FFF;
+    SDA_GPIO_Port->CRH |= 8 << 12;
+}
+
+
+#define SDA_OUT()
+{
+    SCL_GPIO_Port->CRH &= 0XFFFF0FFF;
+    SCL_GPIO_Port->CRH |= 3 << 12;
+}
 
 void IIC_Init()
 {
@@ -22,12 +33,12 @@ void IIC_Init()
     gpio_i2c_scl_pin = SCL_Pin;
     gpio_i2c_sda_gpiox = SDA_GPIO_Port;
     gpio_i2c_sda_pin = SDA_Pin;
-		
+
 }
 
 void delay_us(uint32_t us)
 {
-    while(us--)
+    while (us--)
     {
         __nop();
     }
@@ -37,11 +48,11 @@ int IIC_Start(void)
 {
     SDA_OUT();
     IIC_SDA_HIGH;
-    if(!READ_SDA) return 0;
+    if (!READ_SDA) return 0;
     IIC_SCL_HIGH;
     delay_us(1);
     IIC_SDA_LOW;
-    if(READ_SDA) return 0;
+    if (READ_SDA) return 0;
     delay_us(1);
     IIC_SCL_LOW;
     return 1;
@@ -60,51 +71,51 @@ void IIC_Stop(void)
 
 void IIC_Send_Byte(uint8_t txd)
 {
-    uint8_t t;   
-	SDA_OUT(); 	    
-    IIC_SCL_LOW;//拉低时钟开始数据传输
-    for(t=0;t<8;t++)
-    {              
-        IIC_SDA=(txd&0x80)>>7;
-        txd<<=1; 	  
-		delay_us(1);   
-		IIC_SCL_HIGH;
-		delay_us(1); 
-		IIC_SCL_LOW;	
-		delay_us(1);
-    }	
+    uint8_t t;
+    SDA_OUT();
+    IIC_SCL_LOW; //拉低时钟开始数据传输
+    for (t = 0; t < 8; t++)
+    {
+        IIC_SDA = (txd &0x80) >> 7;
+        txd <<= 1;
+        delay_us(1);
+        IIC_SCL_HIGH;
+        delay_us(1);
+        IIC_SCL_LOW;
+        delay_us(1);
+    }
 }
 
 uint8_t IIC_Read_Byte(uint8_t ack)
 {
-	unsigned char i,receive=0;
-	SDA_IN();//SDA设置为输入
-    for(i = 0; i < 8;i++ )
-	{
+    unsigned char i, receive = 0;
+    SDA_IN(); //SDA设置为输入
+    for (i = 0; i < 8; i++)
+    {
         IIC_SCL_LOW;
         delay_us(2);
-		IIC_SCL_HIGH;
-        receive<<=1;
-        if(READ_SDA)receive++;   
-		delay_us(2); 
-    }					 
+        IIC_SCL_HIGH;
+        receive <<= 1;
+        if (READ_SDA) receive++;
+        delay_us(2);
+    }
     if (ack)
         IIC_Ack(); //发送ACK 
     else
-        IIC_NAck();//发送nACK  
+        IIC_NAck(); //发送nACK  
     return receive;
 }
 
 int IIC_Wait_Ack(void)
 {
-    uint8_t ucErrTime=0;
+    uint8_t ucErrTime = 0;
     SDA_IN();
     IIC_SDA_HIGH;
     delay_us(1);
-    while(READ_SDA)
+    while (READ_SDA)
     {
         ucErrTime++;
-        if(ucErrTime>50)
+        if (ucErrTime > 50)
         {
             IIC_Stop();
             return 0;
@@ -128,105 +139,111 @@ void IIC_Ack(void)
 
 void IIC_NAck(void)
 {
-	SDA_OUT();
-	IIC_SCL_LOW;	// SCL=0
-	IIC_SDA_HIGH;	// SDA=1
-	delay_us(1);
-	iIIC_SCL_HIGH;	// SCL=1
-	delay_us(1);
-	IIC_SCL_LOW;	// SCL=0
-	delay_us(1);
+    SDA_OUT();
+    IIC_SCL_LOW; // SCL=0
+    IIC_SDA_HIGH; // SDA=1
+    delay_us(1);
+    iIIC_SCL_HIGH; // SCL=1
+    delay_us(1);
+    IIC_SCL_LOW; // SCL=0
+    delay_us(1);
 }
 
-uint8_t IICWriteByte(uint8_t dev,uint8_t reg,uint8_t data)
+uint8_t IICWriteByte(uint8_t dev, uint8_t reg, uint8_t data)
 {
     return IICwriteBytes(dev, reg, 1, &data);
 }
 
-uint8_t I2C_ReadOneByte(uint8_t I2C_Addr,uint8_t addr)
+uint8_t I2C_ReadOneByte(uint8_t I2C_Addr, uint8_t addr)
 {
-	unsigned char res=0;
-	
-	IIC_Start();	
-	IIC_Send_Byte(I2C_Addr);	   //发送写命令
-	res++;
-	IIC_Wait_Ack();
-	IIC_Send_Byte(addr); res++;  //发送地址
-	IIC_Wait_Ack();	  
-	//IIC_Stop();//产生一个停止条件	
-	IIC_Start();
-	IIC_Send_Byte(I2C_Addr+1); res++;          //进入接收模式			   
-	IIC_Wait_Ack();
-	res=IIC_Read_Byte(0);	   
-    IIC_Stop();//产生一个停止条件
+    unsigned char res = 0;
 
-	return res;
+    IIC_Start();
+    IIC_Send_Byte(I2C_Addr); //发送写命令
+    res++;
+    IIC_Wait_Ack();
+    IIC_Send_Byte(addr);
+    res++; //发送地址
+    IIC_Wait_Ack();
+    //IIC_Stop();//产生一个停止条件	
+    IIC_Start();
+    IIC_Send_Byte(I2C_Addr + 1);
+    res++; //进入接收模式			   
+    IIC_Wait_Ack();
+    res = IIC_Read_Byte(0);
+    IIC_Stop(); //产生一个停止条件
+
+    return res;
 }
 
-uint8_t IICWriteBit(uint8_t dev,uint8_t reg,uint8_t bitNum,uint8_t data)
+uint8_t IICWriteBit(uint8_t dev, uint8_t reg, uint8_t bitNum, uint8_t data)
 {
     uint8_t b;
     IICreadByte(dev, reg, &b);
-    b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
+    b = (data != 0) ? (b | (1 << bitNum)) : (b &~(1 << bitNum));
     return IICwriteByte(dev, reg, b);
 }
 
-uint8_t IICwriteBytes(uint8_t dev,uint8_t reg,uint8_t length,uint8_t* data)
+uint8_t IICwriteBytes(uint8_t dev, uint8_t reg, uint8_t length, uint8_t *data)
 {
-  
- 	uint8_t count = 0;
-	IIC_Start();
-	IIC_Send_Byte(dev);	   //发送写命令
-	IIC_Wait_Ack();
-	IIC_Send_Byte(reg);   //发送地址
-    IIC_Wait_Ack();	  
-	for(count=0;count<length;count++){
-		IIC_Send_Byte(data[count]); 
-		IIC_Wait_Ack(); 
-	}
-	IIC_Stop();//产生一个停止条件
+    uint8_t count = 0;
+    IIC_Start();
+    IIC_Send_Byte(dev); //发送写命令
+    IIC_Wait_Ack();
+    IIC_Send_Byte(reg); //发送地址
+    IIC_Wait_Ack();
+    for (count = 0; count < length; count++)
+    {
+        IIC_Send_Byte(data[count]);
+        IIC_Wait_Ack();
+    }
+    IIC_Stop(); //产生一个停止条件
 
     return 1; //status == 0;
 }
 
-uint8_t IICwriteBits(uint8_t dev,uint8_t reg,uint8_t bigStart,uint8_t length,uint8_t data)
+uint8_t IICwriteBits(uint8_t dev, uint8_t reg, uint8_t bigStart, uint8_t length, uint8_t data)
 {
-
     uint8_t b;
-    if (IICreadByte(dev, reg, &b) != 0) {
+    if (IICreadByte(dev, reg, &b) != 0)
+    {
         uint8_t mask = (0xFF << (bitStart + 1)) | 0xFF >> ((8 - bitStart) + length - 1);
         data <<= (8 - length);
         data >>= (7 - bitStart);
         b &= mask;
         b |= data;
         return IICwriteByte(dev, reg, b);
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
-uint8_t IICreadBytes(uint8_t dev,uint8_t reg,uint8_t length,uint8_t *data)
+uint8_t IICreadBytes(uint8_t dev, uint8_t reg, uint8_t length, uint8_t *data)
 {
     uint8_t count = 0;
-	
-	IIC_Start();
-	IIC_Send_Byte(dev);	   //发送写命令
-	IIC_Wait_Ack();
-	IIC_Send_Byte(reg);   //发送地址
-    IIC_Wait_Ack();	  
-	IIC_Start();
-	IIC_Send_Byte(dev+1);  //进入接收模式	
-	IIC_Wait_Ack();
-	
-    for(count=0;count<length;count++){
-		 
-		if(count!=length-1){
-            data[count]=IIC_Read_Byte(1);  //带ACK的读数据
+
+    IIC_Start();
+    IIC_Send_Byte(dev); //发送写命令
+    IIC_Wait_Ack();
+    IIC_Send_Byte(reg); //发送地址
+    IIC_Wait_Ack();
+    IIC_Start();
+    IIC_Send_Byte(dev + 1); //进入接收模式	
+    IIC_Wait_Ack();
+
+    for (count = 0; count < length; count++)
+    {
+        if (count != length - 1)
+        {
+            data[count] = IIC_Read_Byte(1); //带ACK的读数据
         }
-		else{
-            data[count]=IIC_Read_Byte(0);	 //最后一个字节NACK
+        else
+        {
+            data[count] = IIC_Read_Byte(0); //最后一个字节NACK
         }
-        
-	}
-    IIC_Stop();//产生一个停止条件
+
+    }
+    IIC_Stop(); //产生一个停止条件
     return count;
 }
